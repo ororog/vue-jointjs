026e002293578a703cf95c8301f49d33dbf17d2a
zuno <zuno@cotobadesign.com>
2018-08-02 18:04:18 +0900

    initial

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <JointJs/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import JointJs from '@/components/JointJs'
+
+export default {
+  name: 'App',
+  components: {
+    JointJs
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/JointJs.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div ref="myholder"></div>
+  </div>
+</template>
+
+<script>
+  import joint from 'jointjs'
+
+  export default {
+    name: 'JointDiagram',
+    mounted(){
+      let graph = new joint.dia.Graph
+      let paper = new joint.dia.Paper({
+        el: this.$refs.myholder,
+        model: graph,
+        width: 600,
+        height: 100,
+        gridSize: 1
+      })
+      let rect = new joint.shapes.standard.Rectangle();
+      rect.position(100, 30);
+      rect.resize(100, 40);
+      rect.attr({
+          body: {
+            fill: 'blue'
+          },
+          label: {
+            text: 'Hello',
+            fill: 'white'
+          }
+      });
+      rect.addTo(graph);
+
+      let rect2 = rect.clone();
+      rect2.translate(300, 0);
+      rect2.attr('label/text', 'World!');
+      rect2.addTo(graph);
+
+      let link = new joint.shapes.standard.Link();
+      link.source(rect);
+      link.target(rect2);
+      link.addTo(graph);
+    }
+  }
+</script>
